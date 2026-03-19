911c08de080a70c7f4640e4898f2c501b1e811ac
bhagenbourger <benoit@hagenbourger.fr>
2026-03-19 16:07:38 +0100

    Add dotfiles and mopidy devcontainer

---
+++ dotfiles/install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+##################################################
+# Get the directory where this script is located
+##################################################
+DOTFILES_DIR=$(cd "$(dirname "$0")" && pwd)
+
+##################################################
+# Install P10k theme if it doesn't exist
+##################################################
+if [ ! -d "${HOME}/powerlevel10k" ]; then
+    current_path=$(pwd)
+    cd /tmp
+    git clone https://github.com/powerline/fonts.git
+    cd fonts
+    ./install.sh
+    cd .. && rm -rf fonts
+    cd $current_path
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
+
+##################################################
+# Symbolically link files to the home directory
+##################################################
+ln -sf "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
+ln -sf "$DOTFILES_DIR/.p10k.zsh" "$HOME/.p10k.zsh"
+ln -sf "$DOTFILES_DIR/.gitignore_global" "$HOME/.gitignore_global"
+
+##################################################
+# Gemini CLI
+##################################################
+current_path=$(pwd)
+USER_LOCAL="$HOME/.local"
+USER_BIN="$USER_LOCAL/node_modules/.bin"
+cd "$USER_LOCAL"
+npm install @google/gemini-cli
+if [ -f ~/.zshrc ] && ! grep -q "$USER_BIN" ~/.zshrc; then
+  echo "export PATH=\"\$PATH:$USER_BIN\"" >> ~/.zshrc
+fi
+cd $current_path
+
+echo "Dotfiles installation complete!"
